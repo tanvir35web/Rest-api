ec0a84780f8af13c19328835ce59fa042c4215e0
tanvir35web <islammdtanvirul@gmail.com>
2024-07-29 23:40:48 +0600

    mmongoDB implemented

--- api/index.js
+++ api/index.js
@@ -1,9 +1,50 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const users = require("../MOCK_DATA.json");
 const fs = require("fs");
+const { type } = require("os");
 
 const app = express();
 const PORT = 8000;
+
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/employees-db-1")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.log("MongoDB Error", error))
+
+//Database Schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    lowercase: true,
+  },
+  jobTitle: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female", "Other"],
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  }
+});
+
+//Model for mongos
+const User = mongoose.model("user", userSchema)
 
 //Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -62,39 +103,47 @@ app
     }
   })
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   // create or add a new user
   const body = req.body;
 
   // validate input data
-  if (!body.first_name ||
-    !body.last_name ||
+  if (!body.firstName ||
+    !body.lastName ||
     !body.email ||
     !body.gender ||
-    !body.ip_address ||
-    !body.job_title ||
-    !body.country ||
-    !body.is_active) {
+    !body.jobTitle ||
+    !body.isActive) {
     return res.status(400).json({ error: "Missing required fields" });
   }
 
-  const newUser = {
-    id: users.length + 1,
-    ...body
-  };
+  const result = await User.create({
+    firstName: body.firstName,
+    lastName: body.lastName,
+    email: body.email,
+    jobTitle: body.jobTitle,
+    gender: body.gender,
+    isActive: body.isActive,
+  })
+  
+  console.log("Result: " + JSON.stringify(result));
+  return res.status(201).json({ massage: "Success" });
 
-  users.push(newUser);
-
-  fs.writeFile("../MOCK_DATA.json", JSON.stringify(users), (error) => {
-    if (error) {
-      return res.status(500).json({ error: "Failed to create user" });
-    }
-    return res.json({
-      status: "User Created Successfully",
-      id: newUser.id
-    });
-  });
-  console.log(body);
+  // const newUser = {
+  //   id: users.length + 1,
+  //   ...body
+  // };
+  // users.push(newUser);
+  // fs.writeFile("../MOCK_DATA.json", JSON.stringify(users), (error) => {
+  //   if (error) {
+  //     return res.status(500).json({ error: "Failed to create user" });
+  //   }
+  //   return res.json({
+  //     status: "User Created Successfully",
+  //     id: newUser.id
+  //   });
+  // });
+  // console.log(body);
 })
 
 
